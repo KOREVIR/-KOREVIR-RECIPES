23dec94ef5999590dba3e8e0060e4d350e478be5
KOREVIR <hsaltik5454@gmail.com>
2026-07-21 23:03:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+const search = document.getElementById("search");
+
+if(search){
+
+search.addEventListener("keyup", function(){
+
+const value = this.value.toLowerCase();
+
+const cards = document.querySelectorAll(".recipe-card");
+
+cards.forEach(card=>{
+
+const text = card.innerText.toLowerCase();
+
+card.style.display = text.includes(value) ? "block" : "none";
+
+});
+
+});
+
+}
+
+});
